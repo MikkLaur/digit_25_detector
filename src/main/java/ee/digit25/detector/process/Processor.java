919e1ab12b565a48e1010cd2053d41a7f293424c
Mikk Laur <mikk.laur@bigbank.ee>
2025-05-07 20:17:52 +0300

    concurrency

--- src/main/java/ee/digit25/detector/process/Processor.java
+++ src/main/java/ee/digit25/detector/process/Processor.java
@@ -10,6 +10,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -17,9 +21,18 @@ import java.util.List;
 public class Processor {
 
     private final int TRANSACTION_BATCH_SIZE = 1000;
+
+    private int threadCount;
+
     private final TransactionRequester requester;
+
     private final TransactionValidator validator;
+
     private final TransactionVerifier verifier;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(
+        Runtime.getRuntime().availableProcessors()
+    );
 
     @Scheduled(fixedDelay = 300) //Runs every 1000 ms after the last run
     public void process() {
@@ -27,14 +40,28 @@ public class Processor {
 
         List<Transaction> transactions = requester.getUnverified(TRANSACTION_BATCH_SIZE);
 
-        for (Transaction transaction : transactions) {
-            if (validator.isLegitimate(transaction)) {
-                log.info("Legitimate transaction {}", transaction.getId());
-                verifier.verify(transaction);
-            } else {
-                log.info("Not legitimate transaction {}", transaction.getId());
-                verifier.reject(transaction);
-            }
-        }
+        List<CompletableFuture<Void>> futures = transactions
+            .stream()
+            .map(transaction -> CompletableFuture.runAsync(
+                () -> {
+                    try {
+                        if (validator.isLegitimate(transaction)) {
+                            log.info("Legitimate transaction {}", transaction.getId());
+                            verifier.verify(transaction);
+                        } else {
+                            log.info("Not legitimate transaction {}", transaction.getId());
+                            verifier.reject(transaction);
+                        }
+                    } catch (Exception e) {
+                        log.error("Error processing transaction {}: {}", transaction.getId(), e.getMessage(), e);
+                    }
+                }, executorService
+            ))
+            .collect(Collectors.toList());
+
+        // Wait for all transactions to be processed
+        CompletableFuture
+            .allOf(futures.toArray(new CompletableFuture[0]))
+            .join();
     }
 }
